7896cf5e8423c60027238e824739e5ad46eee41d
manman <m12345678_0818@qq.com>
2023-11-22 22:27:43 +0800

    待完成的编辑页面

--- app/src/main/java/com/example/study1/MainActivity3.java
+++ app/src/main/java/com/example/study1/MainActivity3.java
@@ -3,12 +3,21 @@ package com.example.study1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    private String[] data ={"Day1","Day2","Day3","Day4","Day5","Day6","Day7","Day8","Day9","Day10"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(
+        MainActivity3.this, android.R.layout.simple_list_item_1,data);
+        ListView listView=(ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
     }
 }
