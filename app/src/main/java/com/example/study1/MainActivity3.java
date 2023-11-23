05e0a5d1fe4f46e3fd5f492f6578f6ac28e5d8af
manman <m12345678_0818@qq.com>
2023-11-23 20:44:43 +0800

    待完成的编辑页面

--- app/src/main/java/com/example/study1/MainActivity3.java
+++ app/src/main/java/com/example/study1/MainActivity3.java
@@ -2,6 +2,7 @@ package com.example.study1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -17,7 +18,14 @@ public class MainActivity3 extends AppCompatActivity {
 
         ArrayAdapter<String>adapter=new ArrayAdapter<String>(
         MainActivity3.this, android.R.layout.simple_list_item_1,data);
-        ListView listView=(ListView) findViewById(R.id.listview);
+        @SuppressLint("MissingInflatedId") ListView listView=(ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+
     }
+//    @Override
+//    protected  void onResume(){
+//        super.onResume();
+//        Object adapter;
+//        listView.setAdapter(adapter);
+//    }
 }
--- app/src/main/java/com/example/study1/MainActivity4.java
+++ app/src/main/java/com/example/study1/MainActivity4.java
@@ -2,13 +2,78 @@ package com.example.study1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    private EditText editText5;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        editText5=(EditText) findViewById(R.id.editText5);
+        String inputTest = load();
+        if(!TextUtils.isEmpty(inputTest)){
+            editText5.setText(inputTest);
+            editText5.setSelection(inputTest.length());
+            Toast.makeText(this,"Restoring succeed",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        String inputTest=editText5.getText().toString();
+        save(inputTest);
+    }
+    public void save(String inputTest){
+        FileOutputStream out =null;
+        BufferedWriter writer=null;
+        try {
+            out=openFileOutput("data", Context.MODE_PRIVATE);
+            writer=new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputTest);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String load(){
+        FileInputStream in =null;
+        BufferedReader reader =null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in =openFileInput("data");
+            reader =new BufferedReader(new InputStreamReader(in));
+            String line="";
+            while ((line =reader.readLine())!=null){
+                content.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            {
+                try {
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+        } return content.toString();
     }
 }
+
